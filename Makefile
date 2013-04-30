0bfdfdb8324000fb43c6ec8eee4c2d835679b660
Bob McWhirter <bob@mcwhirter.org>
2013-04-30 15:27:01 -0400

    Display and tapper.

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+##
+## Requires https://github.com/sudar/Arduino-Makefile
+## 
+## Install it (git clone) under $HOME
+##
+
 BOARD_TAG    = uno
 ARDUINO_PORT = /dev/cu.usb*
 ARDUINO_LIBS = 
--- Toes.ino
+++ Toes.ino
@@ -1,80 +1,175 @@
 #include <LiquidCrystal.h>
+#include <Narcoleptic.h>
 
-int pushButton = 2;
-volatile int lastState = 0;
-volatile int lastPressStart = 0;
 volatile int roundNum = 1;
 volatile int stitchNum = 0;
+volatile int lastActivityTime = 0;
+volatile int lastPressStart = 0;
 
+const int AWAKE = 0;
+const int HALF_SLEEP = 1;
+const int FULL_SLEEP = 2;
+
+const int BACKLIGHT_PIN = 9;
+const int PUSH_BUTTON = 2;
+
+const int NO_PRESS = 0;
+const int SHORT_PRESS = 1;
+const int MEDIUM_PRESS = 2;
+const int LONG_PRESS = 3;
+
+volatile int pressState = NO_PRESS;
+
+volatile int sleepState = AWAKE;
+
+void updateLastActivityTime();
+int timeSinceLastActivity();
+
+void setupButton();
+void setupDisplay();
 void buttonStateChange();
+void updateDisplay(boolean);
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void checkSleep();
+boolean isSleeping();
+
+void triggerHalfSleep();
+void triggerFullSleep();
+void triggerWake();
+
+void onWake();
+
+LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 
 void setup() {
-  pinMode(pushButton, INPUT);
-  //attachInterrupt( 0, buttonStateChange, CHANGE );
-  Serial.begin(9600);
-  Serial.print( "Round: " );
-  Serial.println( roundNum );
-  lcd.clear();
-  delay(3000);
-  lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  lcd.print("Toe-Tapper");
-  lcd.setCursor(0,1);
-  lcd.print("Copyright 2013");
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("McWhirter");
-  lcd.setCursor(0,1);
-  lcd.print("Software, Inc.");
-  delay(1000);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Ready!");
+  setupButton();
+  setupDisplay();
+  updateLastActivityTime();
+}
 
+void setupButton() {
+  pinMode(PUSH_BUTTON, INPUT);
+  attachInterrupt( 0, buttonStateChange, CHANGE );
+}
+
+void setupDisplay() {
+  pinMode(BACKLIGHT_PIN, OUTPUT);   // sets the pin as output
+  analogWrite( BACKLIGHT_PIN, 255 );
+  lcd.begin(16,2);
+  updateDisplay(true);
 }
 
 void loop() {
-  //lcd.setCursor( 0,1);
-  //lcd.print("hello, world!");
-  //int duration = 0;
-  delay(50);
-/*
+  int duration = 0;
+  delay(200);
   if ( lastPressStart > 0 ) {
     duration = millis() - lastPressStart;
-    if ( duration > 1500 ) {
+    if ( pressState == MEDIUM_PRESS && duration > 5000 ) {
+      roundNum = 1;
+      stitchNum = 0;
+      updateDisplay(true);
+      pressState = NO_PRESS;
+    } else if ( pressState == SHORT_PRESS && duration > 2000 ) {
       ++roundNum;
       stitchNum = 0;
-      Serial.print( "Round: " );
-      Serial.println( roundNum );
-      lastPressStart = 0;
+      updateDisplay(true);
+      pressState = MEDIUM_PRESS;
     }
   }
-*/
+
+  checkSleep();
 }
 
-/*
 void buttonStateChange() {
+  updateLastActivityTime();
+
   int duration = 0;
-  int state = digitalRead( pushButton );
+  int state = digitalRead( PUSH_BUTTON );
+
   if ( state == 1 ) {
+    if ( isSleeping() ) {
+      triggerWake();
+      lastPressStart = 0;
+      return;
+    }
+    pressState = SHORT_PRESS;
     lastPressStart = millis();
   } else if ( state == 0 ) {
+    if ( lastPressStart == 0 ) {
+      return;
+    }
     duration = millis() - lastPressStart; 
     lastPressStart = 0;
+ 
+    if ( pressState == SHORT_PRESS && duration > 250 ) {
+      ++stitchNum;
+      updateDisplay(false);
+    }
   }
 
-  if ( duration > 150 ) {
-    if ( duration < 1000 ) {
-      ++stitchNum;
-      Serial.print( "Stitch: " );
-      Serial.println( stitchNum );
-    }
+}
+
+void updateDisplay(boolean full) {
+  updateLastActivityTime();
+  if ( full ) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Row:");
+    lcd.setCursor(0,1);
+    lcd.print("Stitch:");
+  }
+  lcd.setCursor(5,0);
+  lcd.print( roundNum );
+  lcd.setCursor(8,1);
+  lcd.print( stitchNum );
+}
+
+void checkSleep() {
+  int idle = timeSinceLastActivity();
+
+  if ( idle > ( 30 * 1000 ) ) {
+    triggerFullSleep();
+  } else if ( idle > ( 10 * 1000 ) ) {
+    triggerHalfSleep();
+  }
+
+  if ( sleepState == FULL_SLEEP ) { 
+    Narcoleptic.delay( 2000 );
   }
 }
 
 
+void updateLastActivityTime() {
+  lastActivityTime = millis();
+}
 
-*/
+int timeSinceLastActivity() {
+  return millis() - lastActivityTime;
+}
+
+void triggerHalfSleep() {
+  sleepState = HALF_SLEEP;
+  analogWrite( BACKLIGHT_PIN, 100 );
+}
+
+void triggerFullSleep() {
+  sleepState = FULL_SLEEP;
+  analogWrite( BACKLIGHT_PIN, 0 );
+  lcd.noDisplay();
+}
+
+void triggerWake() {
+  sleepState = AWAKE;
+  lcd.display();
+  analogWrite( BACKLIGHT_PIN, 255 );
+}
+
+boolean isSleeping() {
+  return (sleepState != AWAKE);
+}
+
+void onWake() {
+  analogWrite( 9, 255 );
+}
+
+
